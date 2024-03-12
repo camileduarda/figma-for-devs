4bb9e6af4e401890040d317f951681261ad87e6d
Camile Paulista <camile.paulista@alcans.com.br>
2024-03-12 10:24:49 -0300

    subindo o projeto

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 12 Mar 2024 13:12:31 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#524747",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6",
+  "EffectShadowMd": {"shadowType":"dropShadow","radius":4,"color":"#00000040","offsetX":0,"offsetY":4,"spread":0}
+};
